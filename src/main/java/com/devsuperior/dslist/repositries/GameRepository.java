9e98bd0713a3e633fae72540f0b9f1803acd6f0d
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-11-12 10:58:07 -0300

    DTO, Repository, Service and Controller

---
+++ src/main/java/com/devsuperior/dslist/repositries/GameRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dslist.repositries;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.devsuperior.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+}
