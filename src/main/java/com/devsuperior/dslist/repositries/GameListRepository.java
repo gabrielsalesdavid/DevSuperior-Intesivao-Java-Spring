10fcbebfd71d27602835bad71adbb18dfa9dc587
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-11-14 10:59:28 -0300

    move gameList

--- src/main/java/com/devsuperior/dslist/repositries/GameListRepository.java
+++ src/main/java/com/devsuperior/dslist/repositries/GameListRepository.java
@@ -1,7 +1,14 @@
 package com.devsuperior.dslist.repositries;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 import com.devsuperior.dslist.entities.Game;
 
 public interface GameListRepository extends JpaRepository<Game, Long> {
+	
+	@Modifying
+	@Query(nativeQuery = true, value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+	void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
 }
